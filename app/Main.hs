79379e48c17af02243ccac098a3d9c9eeec1d3f0
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2019-03-14 17:11:31 +0100

    annotate symbolic contexts

--- app/Main.hs
+++ app/Main.hs
@@ -7,33 +7,33 @@ import Language.Expr
 -- Examples --------------------------------------------------------------------
 
 
-double_mul :: Expr cxt sxt ('TyInt ':-> 'TyInt)
-double_mul = Lam (Bin Mul (Val 2) (Var Here))
+double_mul :: Expr cxt '[] ('TyInt ':-> 'TyInt)
+double_mul = Lam (Bn Mul (Val 2) (Var Here))
 
 
-double_add :: Expr cxt sxt ('TyInt ':-> 'TyInt)
-double_add = Lam (Bin Add (Var Here) (Var Here))
+double_add :: Expr cxt '[] ('TyInt ':-> 'TyInt)
+double_add = Lam (Bn Add (Var Here) (Var Here))
 
 
-abs :: Expr cxt sxt ('TyInt ':-> 'TyInt)
+abs :: Expr cxt '[] ('TyInt ':-> 'TyInt)
 abs = Lam
-  (If (Bin Lt (Var Here) (Val 0))
+  (If (Bn Lt (Var Here) (Val 0))
     (Un Neg (Var Here))
     (Var Here))
 
 
 abs' :: Expr cxt '[ 'TyInt ] 'TyInt
 abs' =
-  If (Bin Lt (Sym Here) (Val 0))
+  If (Bn Lt (Sym Here) (Val 0))
     (Un Neg (Sym Here))
     (Sym Here)
 
 
-fact :: Expr cxt sxt ('TyInt ':-> 'TyInt)
+fact :: Expr cxt '[] ('TyInt ':-> 'TyInt)
 fact = Lam
-  (If (Bin Eq (Var Here) (Val 0))
+  (If (Bn Eq (Var Here) (Val 0))
     (Val 1)
-    (Bin Mul (App fact (Bin Sub (Var Here) (Val 1))) (Var Here)))
+    (Bn Mul (App fact (Bn Sub (Var Here) (Val 1))) (Var Here)))
 
 
 
