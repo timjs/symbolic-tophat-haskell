49a09812991cc553fb9abcb01c56356d3e537f19
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2019-04-09 17:41:52 +0200

    fix test for new primitive types

--- tst/Test/Exprs.hs
+++ tst/Test/Exprs.hs
@@ -13,27 +13,27 @@ import qualified Language.Expr.Symb as Symb
 -- Functions --
 
 
-double_mul :: Expr cxt '[] ('TyInt ':-> 'TyInt)
+double_mul :: Expr cxt '[] ('TyPrim 'TyInt ':-> 'TyPrim 'TyInt)
 double_mul = Lam (Bn Mul (I 2) (Var Here))
 
 
-double_add :: Expr cxt '[] ('TyInt ':-> 'TyInt)
+double_add :: Expr cxt '[] ('TyPrim 'TyInt ':-> 'TyPrim 'TyInt)
 double_add = Lam (Bn Add (Var Here) (Var Here))
 
 
-abs :: Expr cxt '[] ('TyInt ':-> 'TyInt)
+abs :: Expr cxt '[] ('TyPrim 'TyInt ':-> 'TyPrim 'TyInt)
 abs = Lam
   (If (Bn Lt (Var Here) (I 0))
     (Un Neg (Var Here))
     (Var Here))
 
 
-add :: Expr cxt sxt ('TyInt ':-> ('TyInt ':-> 'TyInt))
+add :: Expr cxt sxt ('TyPrim 'TyInt ':-> ('TyPrim 'TyInt ':-> 'TyPrim 'TyInt))
 add = Lam (Lam (
   Bn Add (Var Here) (Var (There Here))))
 
 
-fact :: Expr cxt '[] ('TyInt ':-> 'TyInt)
+fact :: Expr cxt '[] ('TyPrim 'TyInt ':-> 'TyPrim 'TyInt)
 fact = Lam
   (If (Bn Eq (Var Here) (I 0))
     (I 1)
@@ -43,25 +43,25 @@ fact = Lam
 
 -- Tasks --
 
-enterInt' :: Pretask '[] '[] ('TyTask 'TyInt)
+enterInt' :: Pretask '[] '[] ('TyTask ('TyPrim 'TyInt))
 enterInt' =
   Enter
 
 
-echo :: Pretask '[] '[] ('TyTask 'TyInt)
+echo :: Pretask '[] '[] ('TyTask ('TyPrim 'TyInt))
 echo =
   Enter :>>=
   View (Var Here)
 
 
-add_seq :: Pretask '[] '[] ('TyTask 'TyInt)
+add_seq :: Pretask '[] '[] ('TyTask ('TyPrim 'TyInt))
 add_seq =
   Enter :>>=
   Enter :>>=
   View (Bn Add (Var Here) (Var (There Here)))
 
 
-add_par :: Pretask '[] '[] ('TyTask 'TyInt)
+add_par :: Pretask '[] '[] ('TyTask ('TyPrim 'TyInt))
 add_par = Enter :&&: Enter :>>=
   View (Bn Add (Fst (Var Here)) (Snd (Var Here)))
 
