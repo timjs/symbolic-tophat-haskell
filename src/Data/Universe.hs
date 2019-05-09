83853b3e202bca28ba8bfaaa6e5d24f3f1283cd4
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2019-05-09 11:24:36 +0200

    simplify Universe

--- src/Data/Universe.hs
+++ src/Data/Universe.hs
@@ -1,41 +1,25 @@
 module Data.Universe where
 
 
-
 -- Universes -------------------------------------------------------------------
 
-
-data {- kind -} Sort
-  = Symbolic
-  | Concrete
-
-
 class Universe u where
-  type TypeOf (s :: Sort) (a :: u)
-
-
-type ConcOf a = TypeOf 'Concrete a
-type SymbOf a = TypeOf 'Symbolic a
-
+  type TypeOf (a :: u)
 
 
 -- Contexts --------------------------------------------------------------------
-
 
 data HasType (cxt :: List u) (t :: u) where
   Here :: HasType (a ': cxt) a
   There :: HasType cxt a -> HasType (b ': cxt) a
 
-
 instance Pretty (HasType cxt t) where
   pretty = pretty << idx
-
 
 instance Eq (HasType cxt t) where
   Here    == Here    = True
   There i == There j = i == j
   _       == _       = False
-
 
 instance Ord (HasType cxt t) where
   compare Here Here = EQ
@@ -43,26 +27,18 @@ instance Ord (HasType cxt t) where
   compare (There _) Here = GT
   compare (There i) (There j) = compare i j
 
-
 idx :: HasType cxt t -> Int
 idx = \case
   Here -> 0
   There xs -> 1 + idx xs
 
 
-
 -- Environments ----------------------------------------------------------------
 
+data Env (cxt :: List u) where
+  Nil :: Env '[]
+  Cons :: TypeOf t -> Env ts -> Env (t ': ts)
 
-data Env (s :: Sort) (cxt :: List u) where
-  Nil :: Env s '[]
-  Cons :: TypeOf s t -> Env s ts -> Env s (t ': ts)
-
-
-lookup :: HasType cxt t -> Env s cxt -> TypeOf s t
+lookup :: HasType cxt t -> Env cxt -> TypeOf t
 lookup Here      (Cons x _)  = x
 lookup (There i) (Cons _ xs) = lookup i xs
-
-
-type ConcEnv cxt = Env 'Concrete cxt
-type SymbEnv cxt = Env 'Symbolic cxt
