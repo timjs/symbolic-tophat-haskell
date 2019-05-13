dfeaf99bb036de9e0fcfc88f2e090a39c3571e73
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2019-05-13 20:39:19 +0200

    extend examples

--- test/Test/Exprs.hs
+++ test/Test/Exprs.hs
@@ -7,6 +7,7 @@ import Language.Val (Val, asExpr)
 
 import Control.Monad.List
 import Control.Monad.Supply
+import Control.Monad.Trace
 import Language.Expr
 import Language.Expr.Sim
 
@@ -69,31 +70,64 @@ add_par =
   View (Bn Add (Fst (Var @TyIntInt 0)) (Snd (Var @TyIntInt 0)))
 
 
-guard :: Pretask ('TyTask ('TyPrim 'TyInt))
-guard =
-  Task (Enter @'TyInt) `Then` Lam (
-    If (Bn Gt (Var @('TyPrim 'TyInt) 0) (I 0)) (Task $ View (Var @('TyPrim 'TyInt) 0)) (Task $ Fail)
-  )
+guard1 :: Pretask ('TyTask ('TyPrim 'TyInt))
+guard1 =
+  Enter @'TyInt :>>\
+  If (Bn Gt (Var @('TyPrim 'TyInt) 0) (I 0)) (Task $ View (Var @('TyPrim 'TyInt) 0)) (Task $ Fail)
+
+
+guard2 :: Pretask ('TyTask ('TyPrim 'TyInt))
+guard2 =
+  Edit (Sym @'TyInt -1) :>>\
+  If (Bn Gt (Var @('TyPrim 'TyInt) 0) (I 0)) (Task $ View (Var @('TyPrim 'TyInt) 0)) (Task $ Fail)
 
 
 
 -- Main ------------------------------------------------------------------------
 
 
-type Runner = ListT (Supply Int)
-  -- == Stream Int -> ( List a, Stream Int )
+type Runner = ListT (SupplyT Int Tracer)
+  -- == Stream Int -> ( ( List a, Stream Int ), List (Doc n) )
 
 ids :: Stream Int
 ids = Stream.iterate succ 0
 
-run :: Runner a -> List a
-run r = fst $ runSupply (runListT r) ids
+trace :: Runner a -> List (Doc ())
+trace r = snd $ runTracer (runSupplyT (runListT r) ids)
+
+exec :: Runner a -> List a
+exec r = fst $ fst $ runTracer (runSupplyT (runListT r) ids)
 
 
-test :: Pretask ('TyTask t) -> List ( Val ('TyTask t), List Input, Pred 'TyBool )
-test t0 = run do
-  ( t1, p1 ) <- initialise (Task t0)
-  simulate t1 empty p1
+{-
+>>> print $ trace $ run echo
+[(, ⊠ ▶ λ.□(x0), (True ∧ True))
+,(s0, □(s0) ▶ λ.□(x0), True)
+,(, □(s0), (True ∧ (True ∧ ((True ∧ True) ∧ True))))
+]
 
--- main = do
---   print $ pretty $ test echo
+>>> print $ trace $ run add_seq
+[,(  , ⊠ ▶ λ.⊠ ▶ λ.□((x0 + x1))    , (True ∧ True))
+ ,(s0, □(s0) ▶ λ.⊠ ▶ λ.□((x0 + x1)), True)
+ ,(  , ⊠ ▶ λ.□((x0 + s0))          , (True ∧ (True ∧ ((True ∧ True) ∧ True))))
+ ,(s1, □(s1) ▶ λ.□((x0 + s0))      , True)
+ ,(  , □((s1 + s0))                , (True ∧ (True ∧ ((True ∧ True) ∧ (True ∧ True)))))]
+
+>>> print $ trace $ run add_par
+[,(    , ⊠ ⋈ ⊠ ▶ λ.□((fst x0 + snd x0))        , ((True ∧ True) ∧ (True ∧ True)))
+ ,(F s0, □(s0) ⋈ ⊠ ▶ λ.□((fst x0 + snd x0))    , True)
+ ,(    , □(s0) ⋈ ⊠ ▶ λ.□((fst x0 + snd x0))    , ((True ∧ True) ∧ (True ∧ True)))
+ ,(S s1, ⊠ ⋈ □(s1) ▶ λ.□((fst x0 + snd x0))    , True)
+ ,(    , ⊠ ⋈ □(s1) ▶ λ.□((fst x0 + snd x0))    , ((True ∧ True) ∧ (True ∧ True)))
+ ,(F s2, □(s2) ⋈ ⊠ ▶ λ.□((fst x0 + snd x0))    , True)
+ ,(    , □(s2) ⋈ ⊠ ▶ λ.□((fst x0 + snd x0))    , ((True ∧ True) ∧ (True ∧ True)))
+ ,(S s3, □(s0) ⋈ □(s3) ▶ λ.□((fst x0 + snd x0)), True)
+ ,(    , □((s0 + s3))                          , ((True ∧ True) ∧ ((True ∧ True) ∧ ((True ∧ (True ∧ True)) ∧ ((True ∧ True) ∧ (True ∧ True))))) )
+ ,(F s4, □(s4) ⋈ ⊠ ▶ λ.□((fst x0 + snd x0))    , True)
+ ,(    , □(s4) ⋈ ⊠ ▶ λ.□((fst x0 + snd x0))    , ((True ∧ True) ∧ (True ∧ True)))
+ ,(S s5, □(s2) ⋈ □(s5) ▶ λ.□((fst x0 + snd x0)), True)
+ ,(    , □((s2 + s5))                          , ((True ∧ True) ∧ ((True ∧ True) ∧ ((True ∧ (True ∧ True)) ∧ ((True ∧ True) ∧ (True ∧ True))))) )
+ ,(F s6, □(s6) ⋈ ⊠ ▶ λ.□((fst x0 + snd x0))    , True),(, □(s6) ⋈ ⊠ ▶ λ.□((fst x0 + snd x0)), ((True ∧ True) ∧ (True ∧ True))),(S s7, □(s4) ⋈ □(s7) ▶ λ.□((fst x0 + snd x0)), True),(
+...
+]
+-}
