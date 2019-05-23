73704a8a220e0dc24054c6569a0761a3040db0c7
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2019-05-23 13:18:11 +0200

    simplify Something

--- src/Data/Something.hs
+++ src/Data/Something.hs
@@ -3,34 +3,18 @@ module Data.Something where
 
 -- | Dynamic data abstracting over a constraint of kind `Type -> Constraint`
 data Something c where
-  Some :: ( Typeable a, c a ) => a -> Something c
+  Some :: ( c a ) => a -> Something c
 
 
-instance Pretty (Something Pretty) where
-  pretty (Some x) = sep [ "Some", pretty x ]
+-- instance Pretty (Something Pretty) where
+--   pretty (Some x) = sep [ "Some", pretty x ]
 
 
-instance Eq (Something Eq) where
-  Some x == Some y
-    | Just Refl <- typeOf x ~~ typeOf y = x == y
-    | otherwise = False
+-- instance Eq (Something Eq) where
+--   Some x == Some y
+--     | Just Refl <- typeOf x ~~ typeOf y = x == y
+--     | otherwise = False
 
 
-pack :: forall a c. ( Typeable a, c a ) => a -> Something c
+pack :: forall a c. ( c a ) => a -> Something c
 pack = Some
-
-
-unpack :: forall a c. Typeable a => Something c -> Maybe a
-unpack (Some x)
-  | Just Refl <- typeOf x ~~ r = Just x
-  | otherwise = Nothing
-  where
-    r = typeRep :: TypeRep a
-
-
-unsafeUnpack :: forall a c. Typeable a => Something c -> a
-unsafeUnpack (Some x)
-  | Just Refl <- typeOf x ~~ r = x
-  | otherwise = error $ "Data.Some.unsafeUnpack: Types '" <> show (typeOf x) <> "' and '" <> show r <> "' did not match"
-  where
-    r = typeRep :: TypeRep a
--- src/Data/Steps.hs
+++ src/Data/Steps.hs
@@ -4,31 +4,28 @@ import Data.Something
 
 
 data Steps a
-  = None (Something Pretty)
+  = Non (Something Pretty)
   | End (Something Pretty) a
   | Mid (Something Pretty) (Steps a) (Steps a)
   deriving ( Functor, Foldable, Traversable )
 
 
-record :: Something Pretty -> Steps a -> Steps a
-record h = \case
-  None _ -> None h
-  End _ a -> End h a
-  Mid _ ls rs -> Mid h ls rs
-
-
 instance ( Pretty a ) => Pretty (Steps a) where
   pretty = \case
-    None (Some h) -> sep [ "x", pretty h ]
-    End (Some h) x -> sep [ "-", pretty h, pretty x ]
-    Mid (Some h) ls rs -> split
-      [ sep [ "+", pretty h ]
+    Non (Some p) -> sep [ "x", pretty p ]
+    End (Some p) x -> sep [ "-", pretty p, pretty x ]
+    Mid (Some p) ls rs -> split
+      [ sep [ "+", pretty p ]
       , indent 2 $ pretty ls
       , indent 2 $ pretty rs
       ]
 
 
---
+record :: Something Pretty -> Steps a -> Steps a
+record s = \case
+  Non _ -> Non s
+  End _ x -> End s x
+  Mid _ ls rs -> Mid s ls rs
 
 
 instance Applicative Steps where
@@ -41,12 +38,12 @@ instance Applicative Steps where
 
 
 instance Alternative Steps where
-  empty = None (Some ())
+  empty = Non (Some ())
 
   ls <|> rs = Mid (Some ()) ls rs  --XXX or combine?
 
 
 instance Monad Steps where
-  None h      >>= _ = None h
+  Non s      >>= _ = Non s
   End _ x     >>= k = k x --record (Some x) $ k x
-  Mid h ls rs >>= k = Mid h (ls >>= k) (rs >>= k)
+  Mid s ls rs >>= k = Mid s (ls >>= k) (rs >>= k)
